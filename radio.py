67861290fe26d477d9d47f8662ed0f10ece87a72
Michael Dobosz <doboszmichael@gmail.com>
2017-10-19 10:15:20 -0500

    Update radio.py

--- radio.py
+++ radio.py
@@ -6,7 +6,7 @@
 # 2 : 5V
 # 3 : Contrast (0-5V)*
 # 4 : RS (Register Select)
-# 5 : R/W (Read Write)       - GROUND THIS PIN! We do not want the LCD to send anything to the Pi @ 5v
+# 5 : R/W (Read Write)       - GROUND THIS PIN!
 # 6 : Enable or Strobe
 # 7 : Data Bit 0             - NOT USED
 # 8 : Data Bit 1             - NOT USED
